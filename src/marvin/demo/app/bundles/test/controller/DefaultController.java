9d5f2afcfd2a66aa55a4227e43ad38930f57839e
glingo <cailly.florian.apple@gmail.com>
2016-08-04 17:15:28 +0200

    routine

--- src/marvin/demo/app/bundles/test/controller/DefaultController.java
+++ src/marvin/demo/app/bundles/test/controller/DefaultController.java
@@ -3,14 +3,33 @@ package marvin.demo.app.bundles.test.controller;
 //import com.marvin.old.routing.mapping.Route;
 
 import com.marvin.component.container.awareness.ContainerAware;
+import com.marvin.component.templating.Engine;
+import com.marvin.component.templating.Template;
 import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public class DefaultController extends ContainerAware {
 
 //    @Route(name = "default.home", path = "/")//, reponse="ressources/view/nav.view.twig"
     public void charger() {
-        PrintWriter writer = this.get("print_writer", PrintWriter.class);
-        writer.println("default.charger");
+        try {
+//            writer.println("default.charger");
+            
+            Map<String, Object> context = new HashMap<>();
+            context.put("name", "Mitchell");
+            
+            PrintWriter writer = this.get("print_writer", PrintWriter.class);
+            Engine engine = this.get("templating_engine", Engine.class);
+            Template template = engine.getTemplate("com/marvin/bundle/framework/resources/view/base.html.view");
+            
+            template.evaluate(writer, context);
+            
+        } catch (Exception ex) {
+            Logger.getLogger(DefaultController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 }
